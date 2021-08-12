02f528cfb80fbb096fe61313919c375d00927ebd
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-08-12 20:25:08 +0300

    added a solution to the task

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,17 +1,47 @@
+import countryCardTpl from '../templates/country-card.hbs';
+import countryCardNameTpl from '../templates/country-card-name.hbs';
+
 var debounce = require('lodash.debounce');
 
-import countryCardTpl from '../templates/country-card.hbs';
+// pnotify
+import { defaultModules, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
 
+import '@pnotify/core/dist/BrightTheme.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+// refs
 const inputRef = document.getElementById('input');
 const cardContainerRef = document.getElementById('js-card-container');
 
 function onInput(e) {
   e.preventDefault();
 
-  const form = e.target.value;
+  const form = e.target.value; //запрос который вводят
 
   fetchCountryName(form)
-    .then(renderCountryCard)
+    .then(data => {
+      if (data.length > 10) {
+        error({
+          title: `Too many matches found.`,
+          text: `We found ${data.length} countries. Please enter a more specific query!`,
+          delay: 2500,
+        });
+      } else if (data.length > 2 && data.length < 10) {
+        renderCountryCardName(data);
+      } else if (data.length === 1) {
+        renderCountryCard(data);
+      } else {
+        error({
+          title: `We didn’t find such a country.`,
+          text: `Please check the correctness of the data entered and try again.`,
+          delay: 2500,
+        });
+      }
+    })
     .catch(err => console.error(err));
 }
 inputRef.addEventListener('input', debounce(onInput, 500));
@@ -23,18 +53,29 @@ function fetchCountryName(name) {
   });
 }
 
+// render card with all info
 function renderCountryCard(country) {
-  // if (массив с одной страной) {
-  //     const markup = countryCardTpl(country);
-  // cardContainerRef.innerHTML = markup;
-  // }else if (от 2 - х до 10 - х стран) {
-  //   список имен под инпутом
-  // }else {
-  //   плагин. как установить?
-  // }
-
   const markup = countryCardTpl(country);
-  console.log(markup);
-
   cardContainerRef.innerHTML = markup;
 }
+
+// render only name country
+function renderCountryCardName(country) {
+  const markup = countryCardNameTpl(country);
+  cardContainerRef.innerHTML = markup;
+}
+
+// --------------------------------------------------------------------------
+// const a = fetch('https://restcountries.eu/rest/v2/name/ukraine');
+// console.log(a);
+
+/* ctrl+shift+/ */
+
+// -------------
+// cделать
+// сделать иф/свич
+// как сделать список имен под инпутом
+//  разметка хэндлбар это лишки, список
+// что такое массив
+// сделать бесконечный скрол? или сделать лоад мор?
+// лучше оформить карточки
